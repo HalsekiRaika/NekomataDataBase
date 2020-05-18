875b60703338566fcfff8ee9641cb48774f82af4
ReiRokusanami <reirokusanami.rdh@gmail.com>
2020-05-18 16:38:46 +0900

    Slightly reduced free chat slippage.

--- YoutubeDatabaseController/Util/LiveCheck.cs
+++ YoutubeDatabaseController/Util/LiveCheck.cs
@@ -4,7 +4,7 @@ using YoutubeDatabaseController.Scheme;
 namespace YoutubeDatabaseController.Util {
     public class LiveCheck {
         public static bool IsFreeChat(Item target) {
-            return Regex.IsMatch(target.Snippet.Title, "(フリーチャット|ふり[ー～]ちゃっと|freechat|free chat)", RegexOptions.IgnoreCase);
+            return Regex.IsMatch(target.Snippet.Title, "(フリーチャット|ふり[ー～]ちゃっと|freechat|free chat|みんなの交流場)", RegexOptions.IgnoreCase);
         }
     }
 }
