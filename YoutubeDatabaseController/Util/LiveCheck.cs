264c10733a20168946a339fadec9955204fcd2cb
ReiRokusanami <reirokusanami.rdh@gmail.com>
2020-09-08 02:58:48 +0900

    Added Regex detection patterns. I'll say nothing more.

--- YoutubeDatabaseController/Util/LiveCheck.cs
+++ YoutubeDatabaseController/Util/LiveCheck.cs
@@ -5,7 +5,10 @@ using YoutubeDatabaseController.Scheme;
 namespace YoutubeDatabaseController.Util {
     public static class LiveCheck {
         public static bool IsFreeChat(Item target) {
-            return Regex.IsMatch(target.Snippet.Title, "(フリーチャット|ふり[ー～]ちゃっと|freechat|free chat|みんなの交流場)", RegexOptions.IgnoreCase);
+            return Regex.IsMatch(target.Snippet.Title, 
+                "(フリーチャット|ふり[ー～]ちゃっ[とつト]|freechat|free chat|みんなの交流場|ふり～～ちゃっと|予定表|チャリーフット|といれ|free room|フリーーーーーーーチャット|ふりーーちゃっと|ふり～だむちゃっと|お花見会場|ＣｈａｔＲｏｏｍ|交流（色んな意味で）の場|みんながおしゃべり|チャットルーム|小野町旅館宴会所|フリーチャッツ)",
+                RegexOptions.IgnoreCase
+                );
         }
 
         public static bool IsFinishedLive(RefactorScheme scheme) {
